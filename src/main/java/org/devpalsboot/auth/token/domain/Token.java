c35e36995ab3198220b46eeb8b0a54abd07268cf
Jiwon Park <since1909@daum.net>
2024-07-23 15:03:11 +0900

    Fix: token domain using tdd

--- src/main/java/org/devpalsboot/auth/token/domain/Token.java
+++ src/main/java/org/devpalsboot/auth/token/domain/Token.java
@@ -1,28 +1,40 @@
 package org.devpalsboot.auth.token.domain;
 
+import java.util.Date;
+
 import org.devpalsboot.auth.user.domain.User;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class Token {
     private String accessToken;
     private String refreshToken;
+    private User user;
+    private Date refreshTokenExpiryTime;
 
     @Builder
-    public Token(String accessToken, String refreshToken) {
+    public Token(String accessToken, String refreshToken, User user, Date refreshTokenExpiryTime) {
         this.accessToken = accessToken;
         this.refreshToken = refreshToken;
+        this.user = user;
+        this.refreshTokenExpiryTime = refreshTokenExpiryTime;
     }
 
-    public void reissueAccessToken(String newAccessToken) throws IllegalAccessException {
-        if (!isValidRefreshToken()) {
-            throw new IllegalAccessException("the refresh token is not valid.");
-        }
-        this.accessToken = newAccessToken;
+    /**
+     * token 발급
+     */
+    public void createToken() {
+        throw new RuntimeException("failed to create token");
     }
 
-    private boolean isValidRefreshToken() {
-        // this.refreshToken 검증 로직
-        return true;
+    /**
+     * access token 재발급
+     *
+     * @param newAccessToken
+     */
+    public void reissueAccessToken(String newAccessToken) {
+        throw new RuntimeException("failed to reissue access token.");
     }
 }
