af559a4e5f4781b850defc15686515f3f5923555
Jiwon Park <since1909@daum.net>
2024-07-23 14:31:19 +0900

    Add: Token domain

--- src/main/java/org/devpalsboot/auth/domain/Token.java
+++ src/main/java/org/devpalsboot/auth/domain/Token.java
@@ -1,4 +1,24 @@
 package org.devpalsboot.auth.domain;
 
 public class Token {
+    private User user;
+    private String accessToken;
+    private String refreshToken;
+
+    public Token createToken(User user) {
+        // 새 토큰을 생성한다
+        return null;
+    }
+
+    public void reissueAccessToken(String newAccessToken) throws IllegalAccessException {
+        if (!isValidRefreshToken()) {
+            throw new IllegalAccessException("the refresh token is not valid.");
+        }
+        this.accessToken = newAccessToken;
+    }
+
+    private boolean isValidRefreshToken() {
+        // this.refreshToken 검증 로직
+        return true;
+    }
 }
